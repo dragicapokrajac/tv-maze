a0cb47f756b598281a8e1dc61f7e1f747e976572
dp <dadapokrajac@gmail.com>
2024-07-02 13:31:41 +0200

    data have been fetched

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+   const [shows, setShows] = useState([]);
+
+   useEffect(() => {
+      getShows();
+   }, []);
+
+   const getShows = async () => {
+      try {
+         const response = await axios.get('https://api.tvmaze.com/search/shows?q=all');
+         const data = response.data;
+         setShows(data);
+      } catch (error) {
+         console.error(error);
+      }
+   };
+
    return (
       <div className="App">
-         <h1>App component</h1>
+         <ul>
+            {shows.map(show =>
+               <li key={show.show.id}>
+                  <p>{show.show.name}</p>
+                  <p>{show.score}</p>
+                  <p>{show.show.premiered}</p>
+                  <img src={show.show.image?.medium} />
+               </li>
+            )}
+         </ul>
       </div>
    );
 }
